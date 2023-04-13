2bdce8e3e1cb4e15777e63cbd774f0c8d5425a5f
caioagralemos <caioagralemos@live.com>
2023-04-13 15:25:03 +0200

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,4 @@
+waleska_pre_eataly = 21.38 + 10.15 + 13.10 + 10 + 7.75 + 76.1 + 35 + 9.2 + 6 + 6.8 + 10 + 15.9 + 7.6 + 16.95 + 7.60 + 3.72 + 21.75 + 2 + 62 + 130 + 14.9 + 17 + 70 + 15 + 1.5 + 20.7 + 13.4
+waleska_pos_eataly = 61.5 + 43.25 + 4.75 + 15.5 + 64.85 + 11.20 + 14.75 + 11.2 + 4 + 2 + 20 + 10 + 6 + 82.48
+
+print(f"Waleska:\n\nPré-eataly: {waleska_pre_eataly}\nEataly: {1016 - waleska_pre_eataly}\nPós-eataly: {waleska_pos_eataly}\nTotal: {waleska_pos_eataly + waleska_pre_eataly + 40}\n\n")
